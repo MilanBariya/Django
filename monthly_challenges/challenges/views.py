a6bfc03e57d33dcfa0292b8c410d2ea22191731a
Milan Bariya <milanbariya12@gmail.com>
2023-03-25 15:58:23 +0530

    Add Path converter

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
+def monthly_challenges_by_number(result, month):
+    return HttpResponse(month)
 
 
 def monthly_challenges(resuest, month):
