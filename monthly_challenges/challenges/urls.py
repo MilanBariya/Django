9c192ec568cd95e25bad5441ea4cc079b0ea7d8d
Milan Bariya <milanbariya12@gmail.com>
2023-03-25 15:58:23 +0530

    Add Path converter

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<month>", views.monthly_challenges),
+    path("<int:month>", views.monthly_challenges_by_number),
+    path("<str:month>", views.monthly_challenges),
 ]
